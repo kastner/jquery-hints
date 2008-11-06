6d6951ec4ed2d2eba85992496a6f15411cc8d188
Erik Kastner <kastner@gmail.com>
2008-11-06 15:52:05 -0500

    codez

---
+++ jquery-form-hints.js
@@ -0,0 +1,33 @@
+;(function($) {
+  $.fn.hints = function(options) {
+    var opts = $.extend({}, $.fn.hints.defaults, options);
+
+    return this.each(function() {
+      var form = $(this);
+      
+      $("label", $(this)).each(function() {
+        var label = $(this);
+        var input = $("#" + label.attr('for'));
+
+        var l_o = $.metadata ? $.extend({}, opts, label.metadata()) : opts;
+        var i_o = $.metadata ? $.extend({}, opts, input.metadata()) : opts;
+        
+        var initial = label.hide().text().replace(':', '');
+        
+        input.blur(function() {
+          if (input.val() == '' || input.val() == initial) { input.addClass(l_o['className']).val(initial); }
+        }).focus(function() {
+          if (input.val() == initial) { input.removeClass(l_o['className']).val(''); }
+        }).blur();
+        
+        form.submit(function() {
+          input.focus();
+        });
+      });
+    });
+  };
+  
+  $.fn.hints.defaults = {
+    className: "hinted",
+  }
+})(jQuery);
